2061a7687844e102a5e88d4fc21c578b1ed7dfdb
Jose Carlos Santamaria Poza <jcspoza@gmail.com>
2024-09-06 19:44:36 +0200

    Add files via upload

---
+++ 2425CL0/P_2425CL0_hola.py
@@ -0,0 +1,12 @@
+# Taller Programación y Robótica en CMM BML – 2024 -2025 - Clase 0
+# Resumen: se presenta, pregunta nombre y responde encantado .. con el nombre dicho
+# Creditos : adaptacion de JCSP basado en "Invent with python" ed4 - cap. 2
+# https://inventwithpython.com/invent4thed/chapter2.html
+# Fecha JCSP 2023 02 06
+# Licencia : CC BY-NC-SA 4.0
+# https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es
+
+print('¡Hola mundo! Soy un programa en Python')
+print('¿Cuál es tu nombre?')
+miNombre = input()
+print('Encantado de conocerte, ' + miNombre)
